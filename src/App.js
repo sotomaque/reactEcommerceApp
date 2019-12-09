0533d40f57ef79d4a653d359235cc3a3732f3861
sotomaque <sotomaque@gmail.com>
2019-12-09 11:41:58 -0800

    implemented firebase utils, included ability to store authenticated users into firestore db

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends Component {
   constructor() {
@@ -21,9 +21,37 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-    })
+    // when a user signs in
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // check if theres a user auth
+      if (userAuth) {
+        // get the user ref from our method passing in the user auth
+        // if there is a document there, we will get back the userRef
+        // if there is no document there, we will create a new object and document in that place
+        // and we will still get back that user ref
+        const userRef = await createUserProfileDocument(userAuth);
+    
+        // then we will subscribe (listen for changes) to this user ref for any changes
+        // to that data, 
+
+        // also get back the first state of our data
+        userRef.onSnapshot(snapShot => {
+          // use that data to set our local state
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            });
+          
+          console.log(this.state);
+        });
+  
+      }
+      // if user ever logs out, aka if userAuth is null, set currentUser to null 
+      this.setState({currentUser: userAuth});
+    });
   }
 
   componentWillUnmount() {
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-import FormInput from '../../components/form-input/form-input.component';
-import CustomButton from '../../components/custom-button/custom-button.component';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import { signInWithGoogle } from '../../firebase/firebase.utils.js';
 
