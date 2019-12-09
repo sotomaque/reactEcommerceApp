ceb2e0acab7782d33f18c531e738d35827a861cf
sotomaque <sotomaque@gmail.com>
2019-12-09 13:29:29 -0800

    added redux and implemented user reducer and user actions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
@@ -8,19 +9,17 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
+
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    // destructure
+    const { setCurrentUser } = this.props;
+
     // when a user signs in
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       // check if theres a user auth
@@ -34,21 +33,18 @@ class App extends Component {
         // then we will subscribe (listen for changes) to this user ref for any changes
         // to that data, 
 
-        // also get back the first state of our data
+        // whenever our user snapshot updates
         userRef.onSnapshot(snapShot => {
-          // use that data to set our local state
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data()
-              }
+          // setting the user reducer value with the new object
+          setCurrentUser({
+              id: snapShot.id,
+              ...snapShot.data()   
             });
         });
   
       }
       // if user ever logs out, aka if userAuth is null, set currentUser to null 
-      this.setState({currentUser: userAuth});
+      setCurrentUser(userAuth);
     });
   }
 
@@ -60,7 +56,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header/>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/signin" component={SignInAndSignUpPage} />
@@ -72,4 +68,9 @@ class App extends Component {
  
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -42,7 +42,7 @@ class SignIn extends Component {
     render() {
         return (
            <div className="sign-in">
-                <h2>I already have an account</h2>
+                <h2 className="title">I already have an account</h2>
                 <span>Sign in with your email</span>
 
                 <form onSubmit={this.handleSubmit}>
