b5a96f75d97035c681e91bd78c996a1aa7d94966
sotomaque <sotomaque@gmail.com>
2020-02-27 17:14:02 -0800

    updated components with new redux flow for directory/shop

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,15 +8,13 @@ const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1>{title.toUpperCase()}</h1>
         <div className="preview">
-            {
-                items
-                    .filter((item, index) => index < 4)
-                    .map((item) => (
-                        <CollectionItem key={item.id} item={item} />
-                    ))
-            }
+            {items
+                .filter((item, index) => index < 4)
+                .map((item) => (
+                    <CollectionItem key={item.id} item={item} />
+                ))}
         </div>
     </div>
-)
+);
 
 export default CollectionPreview;
